bb198c2e2ed146ba26da14f841f7d4b5eed78df5
Verna Koskinen <darth.kipsu@gmail.com>
2016-10-24 10:03:42 +0300

    add styles facts

--- src/kipsufi/views/about.clj
+++ src/kipsufi/views/about.clj
@@ -8,7 +8,7 @@
 (defn content []
   [:div.about-page
    [:section.intro
-    [:div.left
+     [:img {:src "images/about.jpg"}]
      [:div.title "About me"]
      [:div.about 
       [:p "I'm Verna Koskinen, a hacker, builder and explorer. I have a previous degree in 
@@ -20,8 +20,40 @@
           I like to get outside with my dog and spouse and discover things within my new home
           country in Switzerland. Nature is really important to me and I think caring for
           the environment and spending time in the nature is one of the best ways to keep ones
-          mind at ease."]]]
-    [:div.right
-     [:img {:src "images/about.jpg"}]]]
+          mind at ease."]]
+     [:img {:src "images/about.jpg"}]]
+
    [:section.bits
-    [:div.title "Facts"]]])
+    [:div.title "Facts"]
+    [:div.subtitle "Currently working on"]
+    [:div.table
+     [:div.tr
+      [:div.td "Workplace:"]
+      [:div.td [:a {:href "//google.com"} "Google"]]]
+     [:div.tr
+      [:div.td "Previous work:"]
+      [:div.td [:a {:href "//mehackit.org"} "Mehackit"]]]
+     [:div.tr
+      [:div.td "Education:"]
+      [:div.td "Bachelor of Science (Computer Science), University of Helsinki"]]
+     [:div.tr
+      [:div.td "Previous education:"]
+      [:div.td "Bachelor of Engineering (Environmental Engineering), Lahti University of Applied Sciences"]]]
+
+    [:div.subtitle "Contact details"]
+    [:div.table
+     [:div.tr
+      [:div.td "Email:"]
+      [:div.td "darth.kipsu@gmail.com"]]
+     [:div.tr
+      [:div.td "Github:"]
+      [:div.td [:a {:href "//github.com/DarthKipsu"} "DarthKipsu"]]]
+     [:div.tr
+      [:div.td "LinkedIn:"]
+      [:div.td [:a {:href "//linkedin.com/in/vernakoskinen"} "Verna Koskinen"]]]
+     [:div.tr
+      [:div.td "Facebook:"]
+      [:div.td [:a {:href "//facebook.com/verna.koskinen"} "Verna Koskinen"]]]
+     [:div.tr
+      [:div.td "Lives in:"]
+      [:div.td "Zürich, Switzerland"]]]]])
