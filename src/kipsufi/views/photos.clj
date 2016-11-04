798a1a4a047462a23a7f197a011f47ca937cbf05
Verna Koskinen <darth.kipsu@gmail.com>
2016-11-04 13:41:49 +0200

    paint countries with photographs in photography

--- src/kipsufi/views/photos.clj
+++ src/kipsufi/views/photos.clj
@@ -1,12 +1,49 @@
 (ns kipsufi.views.photos
-  (:require [kipsufi.svg.europe :as svg]))
+  (:require [clojure.java.io :as io]
+            [kipsufi.svg.europe :as svg])
+  (:import java.io.File))
 
 (def options {:full-page? false
               :key :photos
               :title ":: Photography"
               :css "photos"})
 
+(def photo-dir "clj/images/photography/")
+
+(defn ^:private child-dirs-for
+  "Reads a directory and returns a list of child directories for that directory."
+  [directory]
+  (let [children (fn [file] (and (not= (.getName file) (.getName directory))
+                                 (.isDirectory file)))]
+    (filter children (.listFiles directory))))
+
+(defn ^:private read-directories
+  "Returns a list of immediate child directories for a given path."
+  [path]
+  (let [directory (io/file path)]
+    (map (fn [file] (.getName file)) (child-dirs-for directory))))
+
+(defn without-newline [string]
+    (if (.endsWith string "\n")
+          (.substring string 0 (dec (count string)))
+          string))
+
+(defn ^:private read-file
+  "Reads the contents of a file with the given name, folder and parent path."
+  [path folder file]
+  (try
+    (without-newline
+      (slurp (str path folder "/" file)))
+    (catch Exception e "")))
+
+(defn ^:private list-countries 
+  "Returns a set of country id's with photographs."
+  [directories]
+  (reduce (fn [a-set dir] (conj a-set (read-file photo-dir dir "country"))) #{} directories))
+
 (defn content []
+  (let [directories (read-directories photo-dir)
+        countries (list-countries directories)]
   [:div.photos-page
    [:h1.title "Photography"]
-   (svg/europe-map)])
+   (svg/europe-map countries)]))
